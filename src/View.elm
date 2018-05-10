bab1a92ab05b560ecbef822a17486b121bfd09f0
Kent Kawanaka <meal.is.tharihyu@gmail.com>
2018-05-10 23:01:59 +0900

    Viewを抜本的に書き直し

--- src/View.elm
+++ src/View.elm
@@ -4,6 +4,7 @@ import Model
 import Update
 import Html exposing (..)
 import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 
 
 showCard : Model.Card -> String
@@ -43,104 +44,101 @@ showCard c =
         suit ++ " " ++ number
 
 
+showResult : Maybe Model.Result -> String
+showResult result =
+    case result of
+        Just Model.YouWin ->
+            "You Win!"
+
+        Just Model.DealerWins ->
+            "You Lose!"
+
+        Just Model.YouBust ->
+            "You've Busted!"
+
+        Just Model.DealerBust ->
+            "Dealer Has Busted! You Win!"
+
+        Just Model.Draw ->
+            "Draw"
+
+        Just Model.Error ->
+            "Error!"
+
+        Nothing ->
+            "Error!"
+
+
 view : Model.Model -> Html Model.Msg
 view m =
-    case m.status of
-        Model.Title ->
-            div []
-                [ p []
-                    [ text "Welcome To Black Jack" ]
-                , div []
-                    [ button [ onClick Model.GameStart ]
-                        [ text "Start" ]
-                    ]
-                ]
+    let
+        statusText =
+            case m.status of
+                Model.Title ->
+                    "Welcome To Black Jack"
 
-        Model.UsersTurn ->
-            let
-                dealersCard =
+                Model.UsersTurn ->
+                    "Black Jack: Your Turn. Hit or Stand"
+
+                Model.Over ->
+                    "Black Jack: Game Over" ++ (showResult m.result)
+
+        dealersCards =
+            case m.status of
+                Model.UsersTurn ->
                     List.reverse m.dealer
                         |> List.head
                         |> Maybe.andThen (showCard >> Just)
                         |> Maybe.withDefault ""
+                        |> flip (++) " ?"
 
-                playersCards =
-                    List.reverse m.player |> List.map showCard |> String.join " "
-
-                playersPoint =
-                    Maybe.map toString m.playersPoint |> Maybe.withDefault "Busted!"
-            in
-                div []
-                    [ p []
-                        [ text "Black Jack: Your Turn. Hit or Stand" ]
-                    , div []
-                        [ p []
-                            [ text ("Dealer's Card: " ++ dealersCard ++ " ?") ]
-                        , p []
-                            [ text ("Player's Cards: " ++ playersCards) ]
-                        , p []
-                            [ text ("Player's Point: " ++ playersPoint) ]
-                        ]
-                    , div []
-                        [ button [ onClick Model.Hit ]
-                            [ text "Hit" ]
-                        , button [ onClick Model.Stand ]
-                            [ text "Stand" ]
-                        ]
-                    ]
-
-        Model.Over ->
-            let
-                dealersCards =
+                Model.Over ->
                     List.reverse m.dealer |> List.map showCard |> String.join " "
 
-                playersCards =
-                    List.reverse m.player |> List.map showCard |> String.join " "
+                _ ->
+                    ""
 
-                dealersPoint =
-                    Maybe.map toString m.dealersPoint |> Maybe.withDefault "Busted!"
+        playersCards =
+            List.reverse m.player |> List.map showCard |> String.join " "
 
-                playersPoint =
-                    Maybe.map toString m.playersPoint |> Maybe.withDefault "Busted!"
+        playersPoint =
+            Maybe.map toString m.playersPoint |> Maybe.withDefault "Busted!"
 
-                result =
-                    case m.result of
-                        Just Model.YouWin ->
-                            "You Win!"
+        dealersPoint =
+            Maybe.map toString m.dealersPoint |> Maybe.withDefault "Busted!"
 
-                        Just Model.DealerWins ->
-                            "You Lose!"
-
-                        Just Model.YouBust ->
-                            "You've Busted!"
-
-                        Just Model.DealerBust ->
-                            "Dealer Has Busted! You Win!"
-
-                        Just Model.Draw ->
-                            "Draw"
-
-                        Just Model.Error ->
-                            "Error!"
-
-                        Nothing ->
-                            "Error!"
-            in
-                div []
-                    [ p []
-                        [ text ("Black Jack: Game Over " ++ result) ]
-                    , div []
-                        [ p []
-                            [ text ("Dealer's Cards: " ++ dealersCards) ]
-                        , p []
-                            [ text ("Player's Cards: " ++ playersCards) ]
-                        , p []
-                            [ text ("Dealer's Point: " ++ dealersPoint) ]
-                        , p []
-                            [ text ("Player's Point: " ++ playersPoint) ]
-                        ]
-                    , div []
-                        [ button [ onClick Model.GameStart ]
-                            [ text "Retry" ]
-                        ]
+        buttons =
+            case m.status of
+                Model.Title ->
+                    [ button [ onClick Model.GameStart ]
+                        [ text "Start" ]
                     ]
+
+                Model.UsersTurn ->
+                    [ button [ onClick Model.Hit ]
+                        [ text "Hit" ]
+                    , button [ onClick Model.Stand ]
+                        [ text "Stand" ]
+                    ]
+
+                Model.Over ->
+                    [ button [ onClick Model.GameStart ]
+                        [ text "Retry" ]
+                    ]
+    in
+        div []
+            [ p []
+                [ text statusText ]
+            , div [ hidden <| m.status == Model.Title ]
+                [ p []
+                    [ text ("Dealer's Card: " ++ dealersCards) ]
+                , p []
+                    [ text ("Player's Cards: " ++ playersCards) ]
+                , p []
+                    [ text ("Player's Point: " ++ playersPoint) ]
+                , p [ hidden <| m.status /= Model.Over ]
+                    [ text ("Dealer's Point: " ++ dealersPoint) ]
+                ]
+            , div []
+                buttons
+            ]
